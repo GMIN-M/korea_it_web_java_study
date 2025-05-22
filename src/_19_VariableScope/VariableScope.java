c62055c42c82a6d4e5451c2631dd35373cec9bf4
GMIN-M <zpstls2217@naver.com>
2025-05-22 19:26:12 +0900

    Feat: VariableScope

---
+++ src/_19_VariableScope/VariableScope.java
@@ -0,0 +1,18 @@
+package _19_VariableScope;
+
+public class VariableScope {
+    public static void methodA() {
+        String str = "abc";
+
+    }
+    public static void main(String[] args) {
+        String name = "권규민";
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+//        System.out.println(i);{
+        int age = 27;
+    }
+//        System.out.println(age);
+}
